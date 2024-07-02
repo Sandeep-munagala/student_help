84a0cf9439fb510eadddf2dc7780471fe434789c
moukthikjimmy <moukthikpodicheti@gmail.com>
2024-07-02 20:41:56 +0530

    Updated views.py and made changes to templates

--- student_help/helper/views.py
+++ student_help/helper/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from login.models import UserProfile
 
 @login_required
 def log_home(request):
-    return render(request, 'log_home.html', {})
+    user_profile = UserProfile.objects.get(user=request.user)
+    context = {
+        'user_profile': user_profile,
+    }
+    return render(request, 'log_home.html', context)
+
