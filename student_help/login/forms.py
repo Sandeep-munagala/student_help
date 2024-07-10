8f5455565fdd31a3d4f06eb53f7a92232abc8db7
Sandeep-munagala <munagala.sandeep@learner.manipal.edu>
2024-07-10 13:41:39 +0530

    updtaed

--- student_help/login/forms.py
+++ student_help/login/forms.py
@@ -1,30 +1,47 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import College  # Import your College model here
 
 class EngineeringSignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
     first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
     last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
     dob = forms.DateField(label="", widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'DD-MM-YYYY'}), input_formats=['%d-%m-%Y'])
-    college = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'College Name'}))
+    
+    # Define the college field with a ChoiceField
+    college = forms.ChoiceField(
+        label="",
+        choices=[],  # Choices will be dynamically updated in __init__
+        widget=forms.Select(attrs={'class': 'form-control select-college', 'id': 'id_college'}),  # Add id attribute for JavaScript
+        required=False  # Set to False to allow for the "Others" option
+    )
+
+    # Additional field for entering college name when selecting "Others"
+    other_college = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter College Name', 'style': 'display: none;'}),  # Initially hidden
+        required=False
+    )
+
     year = forms.IntegerField(label="", widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Year of Study'}))
     minor = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Minor'}))
+    
     DEPARTMENT_CHOICES = [
         ('', 'Select Department'), 
         ('Computer Science', 'Computer Science'),
         ('Electrical and Communication', 'Electrical and Communication'),
         ('Mechanical', 'Mechanical'),
         ('Civil', 'Civil'),
-        ('Chemical','Chemical'),
+        ('Chemical', 'Chemical'),
         ('Others', 'Others'),
-        # Add more department options here
     ]
     department = forms.ChoiceField(label="", choices=DEPARTMENT_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'dob', 'college', 'year', 'department', 'minor', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'dob', 'college', 'other_college', 'year', 'department', 'minor', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
         super(EngineeringSignUpForm, self).__init__(*args, **kwargs)
@@ -43,6 +60,20 @@ class EngineeringSignUpForm(UserCreationForm):
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
 
+        # Populate choices for the college field
+        self.fields['college'].choices = self.get_college_choices()
+
+        # Add JavaScript class to the widget
+        self.fields['college'].widget.attrs['class'] = 'form-control select-college'
+
+    def get_college_choices(self):
+        """Return choices for the college dropdown including 'Others'."""
+        choices = [('','Select College')]  # Initial empty choice
+        colleges = College.objects.all()  # Replace with actual queryset
+        for college in colleges:
+            choices.append((college.name, college.name))
+        choices.append(('Others', 'Others'))  # Add 'Others' option
+        return choices
 
 class IntermediateSignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
