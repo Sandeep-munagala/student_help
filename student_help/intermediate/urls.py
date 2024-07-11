48176f1906e0f6a43e266f1fcf69c32430e380c6
Sandeep-munagala <munagala.sandeep@learner.manipal.edu>
2024-07-11 11:37:15 +0530

    minor_change

--- student_help/intermediate/urls.py
+++ student_help/intermediate/urls.py
@@ -3,12 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.intermediate_home, name='intermediate_home'),
-    path('my_network', views.network, name='my_network'),
-    path('computer_science', views.computer_science, name='computer_science'),
-    path('electrical_and_communications', views.electrical_and_communications, name='electrical_and_communications'),
-    path('mechanical', views.mechanical, name='mechanical'),
-    path('civil', views.civil, name='civil'),
-    path('chemical', views.chemical, name='chemical'),
-    path('others', views.others, name='others'),
+    path('my_network/', views.network, name='my_network'),  # Added trailing slash for consistency
+    path('computer_science/', views.computer_science, name='computer_science'),
+    path('electrical_and_communications/', views.electrical_and_communications, name='electrical_and_communications'),
+    path('mechanical/', views.mechanical, name='mechanical'),
+    path('civil/', views.civil, name='civil'),
+    path('chemical/', views.chemical, name='chemical'),
+    path('others/', views.others, name='others'),
 ]
-
